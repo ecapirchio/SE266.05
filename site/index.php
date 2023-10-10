07d1019565f53b5b35b4e7e91a47c9727da74dd3
ecapirchio <122386726+ecapirchio@users.noreply.github.com>
2023-10-10 14:20:00 -0400

    w1

--- site/index.php
+++ site/index.php
@@ -6,4 +6,8 @@
 <h2>Assignment Overview</h2>
 
 <ul>
-<li><a href="../week1/MiniTaskG/index.php">Week 1 - Fizz Buzz</a></li>s
+<li><a href="../week1/MiniTaskC/index.php">Week 1 - Mini Task C</a></li>
+<li><a href="../week1/MiniTaskD/index.php">Week 1 - Mini Task D</a></li>
+<li><a href="../week1/MiniTaskE/index.php">Week 1 - Mini Task E</a></li>
+<li><a href="../week1/MiniTaskF/index.php">Week 1 - Mini Task F</a></li>
+<li><a href="../week1/MiniTaskG/index.php">Week 1 - Mini Task G</a></li>
