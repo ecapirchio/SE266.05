7a1527345c766eb689e152ebdbfbcb9077e7fd66
ecapirchio <122386726+ecapirchio@users.noreply.github.com>
2023-10-09 22:19:55 -0400

    w1

--- week1/MiniTaskE/index.php
+++ week1/MiniTaskE/index.php
@@ -8,11 +8,12 @@
 
 <?php
 
+//Define an array named $task with information about a task
 $task = [
-    'title' => 'Finish homework',
-    'due'   => 'today',
-    'assigned_to' => 'Jeffrey',
-    'completed' => false
+    'title' => 'Finish homework', //Task title
+    'due'   => 'today', //Due date
+    'assigned_to' => 'Jeffrey', //Person assigned to the task
+    'completed' => false //Task completion status
 ];
 
 ?>
@@ -20,9 +21,12 @@ $task = [
 <h1>Task For The Day</h1>
 
 <ul>
+    <!--Display the task title-->
     <li>
         <strong>Name: </strong> <?= $task['title']; ?>
     </li>
 
+    <!--Display the due date for the task-->
     <li>
         <strong>Due Date: </strong> <?= $task['due']; ?>
+    </li>
