0d91d1b8355e22247f892b7538f1f9e0406ea9d2
ecapirchio <122386726+ecapirchio@users.noreply.github.com>
2023-10-10 14:05:26 -0400

    w1

---
+++ site/index.php
@@ -0,0 +1,9 @@
+<h1>PHP and MySQL - Emma Capirchio</h1>
+<p>
+    Welcome to my PHP and MySQL page. You can find an overview of all my working PHP projects along with my code.
+</p>
+
+<h2>Assignment Overview</h2>
+
+<ul>
+<li><a href="../week1/MiniTaskG/index.php">Week 1 - Fizz Buzz</a></li>s
