e766989277b8ca0c8a4a78ad063f005479ac5c71
ecapirchio <122386726+ecapirchio@users.noreply.github.com>
2023-10-09 22:20:57 -0400

    w1

--- week1/MiniTaskC/index.php
+++ week1/MiniTaskC/index.php
@@ -4,30 +4,30 @@
     <meta charset="UTF-8">
     <title>Mini Task C</title>
     <style>
-        /* CSS style for the header element */
+        /*CSS style for the header element*/
         header {
-            background: #e3e3e3; /* Background color */
-            padding: 2em; /* Padding around the header content */
-            text-align: center; /* Center-align the text */
+            background: #e3e3e3; /*Background color*/
+            padding: 2em; /*Padding around the header content*/
+            text-align: center; /*Center-align the text*/
         }
     </style>
 </head>
 <body>
 
 <?php
-
+//Create $animals array
 $animals = [
     'Cat',
     'Dog',
     'Cow'
 ];
 
-// Start a PHP foreach loop to iterate through the $animals array
+//Start a PHP foreach loop to iterate through the $animals array
 foreach ($animals as $animal) : ?>
-    <!-- Start of HTML list item for each animal -->
+    <!--Start of HTML list item for each animal-->
     <li><?= $animal; ?></li>
-    <!-- End of HTML list item -->
-<?php endforeach; // End of the foreach loop ?>
+    <!--End of HTML list item-->
+<?php endforeach; //End of the foreach loop ?>
 
 </body>
 </html>
