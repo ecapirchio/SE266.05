384d0a8d7bf3e7757e957ae1b5ad6ee9034755d2
ecapirchio <122386726+ecapirchio@users.noreply.github.com>
2023-10-09 22:49:33 -0400

    w1

--- week1/MiniTaskD/index.php
+++ week1/MiniTaskD/index.php
@@ -14,13 +14,9 @@
         'assigned_to:' => ' Emma', //Person assigned to the task
         'completed:' => ' yes' //Task completion status
     ];
-
-    //Start a PHP foreach loop to iterate through the $animals array
-    foreach ($tasks as $task) : ?>
-        <!--Start of HTML list item for each animal-->
-        <li><?= $task; ?></li>
-        <!--End of HTML list item-->
-    <?php endforeach; //End of the foreach loop ?>
+    
+    ?>
+    
     <ul>
         <?php foreach ($tasks as $key => $feature) : ?>
             <li><strong><?= $key; ?></strong><?= $feature; ?></li>
