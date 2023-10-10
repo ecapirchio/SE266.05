c1142a49825695cdfea0a46edd0172831bae8d88
ecapirchio <122386726+ecapirchio@users.noreply.github.com>
2023-10-09 22:18:07 -0400

    w1

--- week1/MiniTaskD/index.php
+++ week1/MiniTaskD/index.php
@@ -8,21 +8,28 @@
 
 <?php
 
+//Define an array named $person with information about a person
 $person = [
     'age' => 31,
     'hair' => 'brown',
     'career' => 'web developer'
 ];
 
+//Add a 'name' key to the $person array and set it to 'Jeffrey'
 $person['name'] = 'Jeffrey';
 
+//Remove the 'age' key from the $person array
 unset($person['age']);
 
+//Start a PHP foreach loop to iterate through the $person array
 foreach ($person as $key => $feature) : ?>
+    <!--Start of HTML unordered list for each person's feature-->
     <ul>
+        <!--List item displaying the feature key in bold and its value-->
         <li><strong><?= $key; ?></strong> <?= $feature; ?></li>
     </ul>
-<?php endforeach; ?>
+    <!--End of HTML unordered list-->
+<?php endforeach; //End of the foreach loop ?>
 
 </body>
 </html>
