4547af98bc078085ab858f8ffa10685393ff679a
ecapirchio <122386726+ecapirchio@users.noreply.github.com>
2023-10-16 11:54:26 -0400

    w1

--- week2/process_form.php
+++ week2/process_form.php
@@ -1,4 +1,48 @@
 <?php
+
+    function age($bdate) {
+        $date = new DateTime($bdate);
+        $now = new DateTime();
+        $interval = $now->diff($date);
+        return $interval->y;
+    }
+
+    function isDate($dt) {
+        $date_arr = explode('-', $dt);
+        return checkdate($date_arr[1], $date_arr[2], $date_arr[0]);
+    }
+
+    function poundsToKilograms($pounds) {
+        return $pounds * 0.453592; // 1 pound = 0.453592 kilograms
+    }
+
+    function bmi($ft, $inch, $weight_lbs) {
+        // Convert feet and inches to centimeters
+        $height_cm = ($ft * 30.48) + ($inch * 2.54);
+        
+        // Convert weight from pounds to kilograms
+        $weight_kg = poundsToKilograms($weight_lbs);
+
+        // Calculate BMI
+        if ($height_cm <= 0 || $weight_kg <= 0) {
+            return 0; // Handle invalid input
+        }
+        $bmi = $weight_kg / (($height_cm / 100) * ($height_cm / 100));
+        return $bmi;
+    }
+
+    function bmiDescription($bmi) {
+        if ($bmi < 18.5) {
+            return "Underweight";
+        } elseif ($bmi < 24.9) {
+            return "Normal Weight";
+        } elseif ($bmi < 29.9) {
+            return "Overweight";
+        } else {
+            return "Obese";
+        }
+    }
+
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $first_name = $_POST["first_name"];
         $last_name = $_POST["last_name"];
