5cfe3c9cb9177577e39fbf670dbafb7c9ddd5760
ecapirchio <122386726+ecapirchio@users.noreply.github.com>
2023-10-16 12:25:10 -0400

    w1

--- week2/index.php
+++ week2/index.php
@@ -32,5 +32,12 @@
         
         <input type="submit" value="Submit">
     </form>
+
+    <?php
+    $file = basename($_SERVER['PHP_SELF']);
+    $mod_date=date("F d Y h:i:s A", filemtime($file));
+    echo "File last updated $mod_date";
+    ?>
+    
 </body>
 </html>
