04439902ac017d10e59952ab9862f56196c349f7
ecapirchio <122386726+ecapirchio@users.noreply.github.com>
2023-10-15 23:39:21 -0400

    w1

--- site/index.php
+++ site/index.php
@@ -23,4 +23,7 @@
 </ul>
 
 <?php
-$file = basename($_SERVER['PHPSELF'])
+    $file = basename($_SERVER['PHPSELF']);
+    $mod_date=date("F d y h:i:s A", filemtime($file));
+    echo "File last updated $mod_date";
+?>
