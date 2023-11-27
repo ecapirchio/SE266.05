80aed9afb59875e968810569f6f076df48c73583
ecapirchio <122386726+ecapirchio@users.noreply.github.com>
2023-11-27 16:46:41 -0500

    w3

---
+++ week6/login.php
@@ -0,0 +1,52 @@
+<?php
+session_start();
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // Validate the login credentials
+    $username = $_POST['username'];
+    $passkey = $_POST['passkey'];
+
+    // Perform authentication (you should replace this with your authentication logic)
+    if (authenticateUser($username, $passkey)) {
+        // Authentication successful
+        $_SESSION['username'] = $username;
+        header("Location: search.php");
+        exit();
+    } else {
+        // Authentication failed
+        $error_message = "Invalid username or passkey";
+    }
+}
+
+// HTML form for login
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Login</title>
+</head>
+<body>
+    <h2>Login</h2>
+    <?php if(isset($error_message)) { echo "<p>$error_message</p>"; } ?>
+    <form method="post" action="">
+        <label for="username">Username:</label>
+        <input type="text" id="username" name="username" required><br>
+
+        <label for="passkey">passkey:</label>
+        <input type="passkey" id="passkey" name="passkey" required><br>
+
+        <input type="submit" value="Login">
+    </form>
+</body>
+</html>
+
+<?php
+// Replace this function with your actual authentication logic
+function authenticateUser($username, $passkey) {
+    // Your authentication logic here (e.g., check against a database)
+    // Return true if authentication succeeds, false otherwise
+    // Remember to encrypt and compare passkeys securely
+    return true;
+}
+?>
---
+++ week6/search.php
@@ -0,0 +1,27 @@
+<?php
+session_start();
+
+// Check if the user is logged in
+if (!isset($_SESSION['username'])) {
+    header("Location: login.php");
+    exit();
+}
+
+// Your HTML and search functionality here
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Search</title>
+</head>
+<body>
+    <h2>Welcome, <?php echo $_SESSION['username']; ?>!</h2>
+    
+    <!-- Your search form and patient listing here -->
+
+    <br>
+    <a href="logoff.php">Log Off</a>
+    <a href="viewPatients.php">Previous Patient Listing</a>
+</body>
+</html>
