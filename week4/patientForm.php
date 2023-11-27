0d9e2816e412fa924a7b5dbc1264e9570e39be1a
ecapirchio <122386726+ecapirchio@users.noreply.github.com>
2023-11-27 14:36:53 -0500

    w3

--- week4/patientForm.php
+++ week4/patientForm.php
@@ -1,47 +1,49 @@
 <?php
-// patientForm.php
 
-include (__DIR__ . '/model_patients.php');
+    include (__DIR__ . '/model_patients.php');
 
-function age($birth_date) {
-    $date = new DateTime($birth_date);
-    $now = new DateTime();
-    $interval = $now->diff($date);
-    return $interval->y;
-}
-
-$error = 0;
-$errormsg = "";
-$now = new DateTime;
-
-if (isset($_POST['submitBtn'])) {
-    $first_name = filter_input(INPUT_POST, 'first_name');
-    $last_name = filter_input(INPUT_POST, 'last_name');
-    $married = isset($_POST['married']) ? 1 : 0; // Use 1 if checked, 0 if not
-    $birth_date = filter_input(INPUT_POST, 'birth_date');
-
-    if (age($birth_date) > 120) {
-        $error += 1;
-        $errormsg .= "Age cannot be more than 119\n";
-    }
-    if ($birth_date > $now->format('Y-m-d')) {
-        $error += 1;
-        $errormsg .= "Cannot enter a future date\n";
-    }
-    if ($error != 0) {
-        echo $errormsg;
-        exit; // Stop execution on error
+    function age($bday) { #function to convert the date of birth to an actual age 
+        $date = new DateTime($bday);
+        $now = new DateTime();
+        $interval = $now->diff($date);
+        return $interval->y;
     }
 
-    $result = addPatient($first_name, $last_name, $married, $birth_date);
-    
-    if ($result === 'Data Added') {
-        header("Location: viewPatients.php");
-        exit;
+    $error = 0; #setting variables
+    $errormsg = "";
+    $now = new DateTime;
+    if (isset($_POST['submitBtn'])){ #if statement to check if areas are set correctly
+
+        $fname = filter_input(INPUT_POST,'fname');
+        $lname = filter_input(INPUT_POST,'lname');
+        $married = filter_input(INPUT_POST,'married');
+        if ($married == true){ 
+            $married = 1;
+        }
+        if ($married == false){
+            $married = 0;
+        }
+        $bday = filter_input(INPUT_POST,'bday'); #All validation for patient
+        if (age($bday) > 120){
+            $error += 1;
+            $errormsg .= "Age cannot be more than 119\n";
+        }
+        if ($bday > $now){
+            $error += 1;
+            $errormsg .= "Cannot enter a future date\n";
+        }
+        if ($error !=0){ #if there are errors it will display all errors
+            echo $errormsg;
+        }
+        if ($error == 0){   #Final output if no errors
+            header("Location: viewPatients.php");
+        }
+        if(isset($_POST['submitBtn']) && ($error == 0)){
+            addPatient($fname, $lname, $married, $bday);
+        }
     }
-}
+
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -50,26 +52,30 @@ if (isset($_POST['submitBtn'])) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Patient Intake Form</title>
     <h1>Patient Intake Form</h1>
+    <form method="post" action="patientForm.php">
 </head>
 <body>
-    <form action="patientForm.php" method="post">
-        <!-- All inputs for the patient -->
-        <label for="first_name">First Name:</label>
-        <input type="text" name="first_name" required>
+    <form action="index.php" method="post"> <!-- All inputs for patient -->
+        <label for="fname">First Name:</label>
+        <input type="text" name="fname" required >
         <br><br>
-        <label for="last_name">Last Name:</label>
-        <input type="text" name="last_name" required>
+        <label for="lname">Last Name:</label>
+        <input type="text" name="lname" required>
         <br><br>
         <label for="married">Married:</label>
-        <input type="checkbox" name="married">
+        <input type="checkbox" name="married" >
         <br><br>
-        <label for="birth_date">Birth Date:</label>
-        <input type="date" name="birth_date" required>
+        <label for="bday">Birth Date:</label>
+        <input type="date" name="bday" required>
         <br><br>
         <input type="submit" name="submitBtn"/> <!-- Submit Button -->
-    </form>
-    <br><br>
-    <a href="viewPatients.php">View Patients</a>
+        </form>
+        <?php
+            $file = basename($_SERVER['PHP_SELF']);
+            $mod_date=date("F d Y h:i:s A", filemtime($file));
+            echo "<br>File last updated $mod_date";
+        ?>
+<br><br>
+<a href="viewPatients.php">View Patients</a>
 </body>
 </html>
-?>
