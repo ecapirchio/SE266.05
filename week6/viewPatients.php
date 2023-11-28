af2bd384fe03660aca5cf6cfb992c5b89db52587
ecapirchio <122386726+ecapirchio@users.noreply.github.com>
2023-11-28 14:15:38 -0500

    w8

--- week6/viewPatients.php
+++ week6/viewPatients.php
@@ -51,7 +51,7 @@ $patients = searchPatients($first_name, $last_name, $married);
 <head>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>Week4 Patients</title>
+<title>Week 6 Patient Form</title>
 </head>
 
 <body>
