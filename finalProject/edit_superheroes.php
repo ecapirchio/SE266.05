10c462f10ee99c8187760d451d54ea6264192292
ecapirchio <122386726+ecapirchio@users.noreply.github.com>
2023-12-10 14:11:04 -0500

    final

--- finalProject/edit_superheroes.php
+++ finalProject/edit_superheroes.php
@@ -58,6 +58,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Superhero Form</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
 
